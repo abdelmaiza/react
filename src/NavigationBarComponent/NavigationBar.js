e533aafad35703f4c992dab08c677b66e4980ebf
maiza <abdelmaiza33@gmail.com>
2023-02-27 05:02:01 +0100

    add style

--- src/NavigationBarComponent/NavigationBar.js
+++ src/NavigationBarComponent/NavigationBar.js
@@ -6,26 +6,20 @@ import './NavigationBar.css';
 
 function NavigationBar() {
     return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                <FontAwesomeIcon icon={faHome} />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <FontAwesomeIcon icon={faHome} />
+            </a>
+            <div className="d-flex justify-content-end">
+              <div className="navbar-nav">
+                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="#">Contact</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      );
 }
 
 export default NavigationBar;
