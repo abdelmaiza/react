a4fedbd0cd2b8189f9a2a034a8de1c278b4a5996
maiza <abdelmaiza33@gmail.com>
2023-02-27 04:48:18 +0100

    add style

--- src/NavigationBarComponent/NavigationBar.js
+++ src/NavigationBarComponent/NavigationBar.js
@@ -5,20 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavigationBar.css';
 
 function NavigationBar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="/">Accueil</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/a-propos">À propos</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/contact">Contact</a>
-          </li>
-        </ul>
+    return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                <FontAwesomeIcon icon={faHome} />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
