fdb1d34a70ac8eb88a387e36b7fc5ec38ba3b903
maiza <abdelmaiza33@gmail.com>
2023-02-27 04:19:09 +0100

    add style

--- src/NavigationBarComponent/NavigationBar.js
+++ src/NavigationBarComponent/NavigationBar.js
@@ -5,10 +5,6 @@ import './NavigationBar.css';
 function NavigationBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
-      <a className="navbar-brand" href="/">Mon site web</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
