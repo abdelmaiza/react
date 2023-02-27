e473004b35137115f5e54b2d4109723dc7611458
maiza <abdelmaiza33@gmail.com>
2023-02-27 04:14:10 +0100

    add style

--- src/NavigationBarComponent/NavigationBar.js
+++ src/NavigationBarComponent/NavigationBar.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavigationBar.css';
 
 function NavigationBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
-      <a className="navbar-brand" href="/">My Website</a>
+      <a className="navbar-brand" href="/">Mon site web</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/">Home</a>
+            <a className="nav-link" href="/">Accueil</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/about">About</a>
+            <a className="nav-link" href="/a-propos">À propos</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/contact">Contact</a>
