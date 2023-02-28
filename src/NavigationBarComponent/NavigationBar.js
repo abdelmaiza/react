d8403ede627b70fa54bf1732472f4add95aa3b10
maiza <abdelmaiza33@gmail.com>
2023-02-28 03:56:20 +0100

    add new button

--- src/NavigationBarComponent/NavigationBar.js
+++ src/NavigationBarComponent/NavigationBar.js
@@ -5,43 +5,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavigationBar.css';
 
 function NavigationBar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
+   return (
+     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+       <div className="container-fluid">
+         <div className="col-sm-6 col-md-2">
             <a className="navbar-brand" href="#">
               <FontAwesomeIcon icon={faHome} />
             </a>
-            <div className="d-flex justify-content-end">
-              <div className="navbar-nav">
-                <li className="nav-item">
-                   <a className="nav-link" href="#">
-                     <FontAwesomeIcon icon={faSearch} />
-                     <span className="nav-link-text">Ma recherche</span>
-                   </a>
-                </li>
-                <li className="nav-item">
-                   <a className="nav-link" href="#">
-                     <FontAwesomeIcon icon={faHeart} />
-                     <span className="nav-link-text">Mes favoris</span>
-                   </a>
-                </li>
-                <li className="nav-item">
-                   <a className="nav-link" href="#">
-                     <FontAwesomeIcon icon={faEnvelope} />
-                     <span className="nav-link-text">Messages</span>
-                   </a>
-                </li>
-                <li className="nav-item">
-                   <a className="nav-link" href="#">
-                     <FontAwesomeIcon icon={faSignInAlt} />
-                     <span className="nav-link-text">Se connecter</span>
-                   </a>
-                </li>
-              </div>
-            </div>
-          </div>
-        </nav>
-      );
+            <button className="btn btn-primary ms-auto" type="button">
+              Déposer une annonce
+            </button>
+         </div>
+         <div className="d-flex justify-content-end">
+           <div className="navbar-nav">
+             <li className="nav-item">
+               <a className="nav-link" href="#">
+                 <FontAwesomeIcon icon={faSearch} />
+                 <span className="nav-link-text">Ma recherche</span>
+               </a>
+             </li>
+             <li className="nav-item">
+               <a className="nav-link" href="#">
+                 <FontAwesomeIcon icon={faHeart} />
+                 <span className="nav-link-text">Mes favoris</span>
+               </a>
+             </li>
+             <li className="nav-item">
+               <a className="nav-link" href="#">
+                 <FontAwesomeIcon icon={faEnvelope} />
+                 <span className="nav-link-text">Messages</span>
+               </a>
+             </li>
+             <li className="nav-item">
+               <a className="nav-link" href="#">
+                 <FontAwesomeIcon icon={faSignInAlt} />
+                 <span className="nav-link-text">Se connecter</span>
+               </a>
+             </li>
+           </div>
+         </div>
+       </div>
+     </nav>
+   );
 }
 
 export default NavigationBar;
